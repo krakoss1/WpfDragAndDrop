77d203af7f138285d2f4e69076725cf19f5f8fef
krrakos <www@krakoss.ru>
2022-05-13 10:33:22 +0500

    Добавьте файлы проекта.

---
+++ WpfDragAndDrop/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDragAndDrop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CanvasImages_Drop(object sender, DragEventArgs e)
+        {
+            canvasImages.Children.RemoveAt(0);
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                string filePath = files[0];
+                myImg.Height = 200;
+                myImg.Source = new BitmapImage(new Uri(filePath, UriKind.Absolute));
+                canvasImages.Children.Add(myImg);
+            }
+        }
+    }
+}
